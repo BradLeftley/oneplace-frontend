dd1d8cf572f3338243423d96ac17ff30cab1d11f
Bradley Leftley <bradleyleftley@Bradleys-MacBook-Pro-2.local>
2022-05-08 15:29:54 +0100

    install apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,23 @@ import {
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "http://localhost:4000/graphql"
+});
 
 export const App = () => (
+  <ApolloProvider client={client}>
   <ChakraProvider theme={theme}>
-      
+  hello
   </ChakraProvider>
+  </ApolloProvider>
 )
